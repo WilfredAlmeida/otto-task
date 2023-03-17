8b340e3e138dfd4f54f4a5c0fe86923f762ca18c
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-03-17 14:14:55 +0530

    chore: Cleaned boilerplace comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:otto_task/views/screens/gallery_homepage.dart';
 import 'package:otto_task/views/screens/login_screen.dart';
 
 void main() {
@@ -10,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Photo Gallery',
+      title: 'Otto Gallery',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
